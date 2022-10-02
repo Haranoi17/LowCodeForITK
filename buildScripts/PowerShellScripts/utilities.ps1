241046a8fec58ad8c0266ef007a1b053e2df7eec
kamil <kamilszczepan1999@gmail.com>
2022-10-02 17:46:17 +0200

    remove script

--- buildScripts/PowerShellScripts/utilities.ps1
+++
@@ -1,8 +0,0 @@
-function Check-Lockation 
-{
-    $current_folder = Get-Location | Split-Path -Leaf
-    if($current_folder -ne "LowCodeForITK")
-    {
-        throw "Wrong location location is " + $current_folder + " and I need /.../LowCodeForITK"
-    }
-}
